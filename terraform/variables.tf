3a6c9cfd92c8cefb544fd169c54c62799a966d63
Annie-Mei Forster <annie-meiforster@Annie-Meis-MacBook-Pro-2.local>
2024-11-16 17:32:59 +1100

    Added remote backend module and configured main configuration file

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {}
+variable "iam_user_name" {}
+variable "bucket_name" {}
+variable "table_name" {}
+variable "root_domain" {}
+variable "dns_record_ttl" {}
+variable "website_bucket" {}
+variable "force_destroy" {}
+variable "versioning_enabled" {}
+variable "index_document" {}
